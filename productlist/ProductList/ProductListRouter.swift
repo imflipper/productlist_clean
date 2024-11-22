1f1b2acd0359c54d01801f28c6a9d0381aaaace7
vbarskov <127094107+vbarskov@users.noreply.github.com>
2024-11-22 13:44:05 +0300

    init

---
+++ productlist/ProductList/ProductListRouter.swift
@@ -0,0 +1,43 @@
+//
+//  ProductListRouter.swift
+//
+//  Created by flappa on 04.11.2024.
+//
+
+import UIKit
+
+
+protocol ProductListRoutingLogic {
+    func routeBack(toRoot: Bool)
+}
+
+protocol ProductListDataPassing {
+    var dataStore: ProductListDataStore? { get }
+}
+
+final class ProductListRouter: ProductListRoutingLogic, ProductListDataPassing {
+    
+    // MARK: - Public Properties
+    
+    weak var parentController: UIViewController?
+    weak var viewController: ProductListViewController?
+    var dataStore: ProductListDataStore?
+    
+    // MARK: - Private Properties
+    
+    //
+    
+    // MARK: - Routing Logic
+    
+    func routeBack(toRoot: Bool) {
+        
+    }
+    
+    // MARK: - Navigation
+    
+    //
+    
+    // MARK: - Passing data
+    
+    //
+}
---
+++ productlist/UI/UIImage+Rezise.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+Rezise.swift
+//
+//  Created by flappa on 05.11.2024.
+//
+
+import UIKit
+
+public extension UIImage {
+    func imageResized(to size: CGSize) -> UIImage {
+        let aspectWidth = size.width / self.size.width
+        let aspectHeight = size.height / self.size.height
+        let aspectRatio = min(aspectWidth, aspectHeight)
+        
+        let newSize = CGSize(width: self.size.width * aspectRatio, height: self.size.height * aspectRatio)
+        
+        return UIGraphicsImageRenderer(size: newSize).image { _ in
+            draw(in: CGRect(origin: .zero, size: newSize))
+        }
+    }
+}
